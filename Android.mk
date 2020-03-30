e491ae02c723ec55424c8020cfbb358fa8cbf4d8
Sagarrokade006 <sagarrokade006@gmail.com>
2020-03-30 08:28:01 +0000

    add hdmi-cec

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-display))
-
 ifeq ($(call is-board-platform-in-list, msm8996),true)
     TARGET_USES_SDM = true
 else
@@ -25,6 +23,4 @@ else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
-endif
-
 endif
